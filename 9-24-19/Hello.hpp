a80a6ab10e6470e31950dfbf039a7260c0eac775
Jacob Christensen <jacob.h.christensen@gmail.com>
2019-09-24 10:49:31 -0600

    Multiple instantiancions

---
+++ 9-24-19/Hello.hpp
@@ -0,0 +1,21 @@
+#ifndef HELLO_HPP
+#define HELLO_HPP
+
+#include <string>
+
+class Hello
+{
+public:
+  Hello();
+
+protected:
+  // Intentionally leaving this empty... stay tuned. Don't drop the class.
+
+private:
+  std::string name;
+
+};
+
+
+
+#endif
--- 9-24-19/main.cpp
+++ 9-24-19/main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
-
+#include "Hello.hpp"
 
 using namespace std;
 
 
 int main()
 {
-	cout << "Doing something now" << endl;
-	return 0;
+  cout << "Doing something now" << endl;
+  int number;
+  Hello greeting;
+  int anotherNumber;
+  Hello bonjour;
+  Hello h1;
+  Hello h2;
+  Hello h3;
+  Hello h4;
+  Hello h5;
+  Hello h6;
+  Hello h7;
+
+
+  return 0;
 }
